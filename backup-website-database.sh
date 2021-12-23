8a2d5efdd4414e0388316774a0c5a300d1a841ad
Alfio <alfio@industria01.it>
2021-12-23 17:24:12 +0100

    comment edited

--- backup-website-database.sh
+++ backup-website-database.sh
@@ -9,13 +9,13 @@
 # DESCRIPTION
 # This script dump the database and create a tar archive from source into destination with database e files.
 # To exclude files or directory: create an .excluded file with files and directories list (one for line) into backup
-# destination directory.
+# directory destination.
 # 
 # INSTALLATION
-# -`sudo chown -R root: /path/to/backups/example.com` (optional to secure database and user password)
-# -`sudo chmod 600 /path/to/backups/example.com/.backup-config` (optional to secure database and user password)
-# - edit config parameters
-# -`sudo chmod 754 /path/to/backup-website-database.sh`
+# -sudo chown -R root: /path/to/backups/example.com (optional to secure database and user password)
+# -sudo chmod 600 /path/to/backups/example.com/.backup-config (optional to secure database and user password)
+# -edit config parameters
+# -sudo chmod 754 /path/to/backup-website-database.sh
 # 
 # AUTHOR: 
 # backup-website-database.sh is written by Alfio Salanitri www.alfiosalanitri.it and are licensed under the MIT License.
